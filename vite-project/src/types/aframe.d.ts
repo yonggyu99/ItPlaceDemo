aadab4b55aaa00ac5db371fae8a651f2350c018c
박용규 <yoyo5411@naver.com>
2025-07-12 16:59:08 +0900

    aframe.js

---
+++ vite-project/src/types/aframe.d.ts
@@ -0,0 +1,11 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+      'a-scene': any;
+      'a-assets': any;
+      'a-entity': any;
+      'a-camera': any;
+      'a-light': any;
+      'a-sky': any;
+    }
+  }
+  
